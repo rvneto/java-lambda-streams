a50ccf8ba17f6ab455466da586e2b812c32fdfff
roberto <rvneto.rs@gmail.com>
2024-06-05 17:53:23 -0300

    Commit inicial

---
+++ src/main/java/com/roberto/java/lambdastreams/JavaLambdaStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.java.lambdastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLambdaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLambdaStreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roberto/java/lambdastreams/JavaLambdaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.java.lambdastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLambdaStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
